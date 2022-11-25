e6d97fa23825d54a358c302c663bb4255c0f394e
EtkarP <etkar.pruuli@outlook.com>
2022-11-25 21:47:17 +0100

    Classes, api feching and converting data

--- wdt_app.js
+++ wdt_app.js
@@ -1,1 +1,30 @@
+class Employee {
+  constructor(name, surename) {
+      this.name = name;
+      this.surename = surename;
+   }
+}
+class StaffMember extends Employee {
+  constructor(name, surename, photo, email) {
+    super(name, surename)
+      this.photo = photo
+      this.email = email
+    }
+}
+class DeliverDriver extends Employee {
+  constructor(name, surename, vehicle, telephone, deliveryAddress){
+    super(name, surename)
+      this.vehicle = vehicle
+      this.telephone = telephone
+      this.deliveryAddress = deliveryAddress
+  }
+}
+
+
+async function staffUserGet() {
+  const response = await fetch('https://randomuser.me/api/?results=5&inc=name,email,picture,')
+  const data = await response.json()
+  const staff = data.results.map(user => new StaffMember(user.name.first, user.name.last, user.picture.large, user.email))
+  return staff
+}
 
