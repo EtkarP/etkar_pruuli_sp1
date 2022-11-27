4fcb9766a33630496f402c95e731d2fe4ab3f581
EtkarP <etkar.pruuli@outlook.com>
2022-11-27 14:29:33 +0100

    outfunction with time calculating w/moment.js

--- wdt_app.js
+++ wdt_app.js
@@ -21,14 +21,14 @@ class DeliverDriver extends Employee {
 }
 
 async function staffUserGet() {
-  const response = await fetch('https://randomuser.me/api/?results=5&inc=name,email,picture,')
+  const response = await fetch('https://randomuser.me/api/?results=5&inc=name,email,picture')
   staff = await response.json()
-  staff = staff.results.map(staffMember => 
+  staff = await staff.results.map(staffMember => 
   new StaffMember(staffMember.name.first, staffMember.name.last, staffMember.picture.medium, staffMember.email))
   tableData=""
   staff.map(staffMembers => {
     tableData += 
-    `<tr>
+    `<tr id="stafftablerow">
     <td><img src="${staffMembers.photo}"></td>
     <td>${staffMembers.name}</td>
     <td>${staffMembers.surename}</td>
@@ -39,5 +39,21 @@ async function staffUserGet() {
     <td id="expectedreturntime"></td>
     </tr>`})
     document.getElementById("staffmembers").innerHTML = tableData
-}
+
+    $(document).ready(function(){
+      $(document.body).on('click', '#stafftablerow', function(){
+        $(this).toggleClass('selected')
+      })
+      
+      $("#clockout").click(function(){
+        let absence = parseInt(prompt("Please enter the duration of absence in minutes"))
+        let selected = $(".selected")
+        selected.find("#status").text("Out")
+        selected.find("#outtime").text(moment().format('HH:mm'))
+        selected.find("#duration").text(absence)
+        selected.find("#expectedreturntime").text(moment().add(absence, 'minutes').format('HH:mm'))
+      })
+    })
+  }
+
 staffUserGet()
