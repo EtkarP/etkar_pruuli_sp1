5c27628c5080fccf8323239e32ca79d3f28b4122
EtkarP <etkar.pruuli@outlook.com>
2022-11-29 18:56:07 +0100

    toast completed

--- wdt_app.js
+++ wdt_app.js
@@ -1,41 +1,34 @@
 class Employee {
   constructor(name, surename) {
-      this.name = name;
-      this.surename = surename;
+    this.name = name;
+    this.surename = surename;
    }
 }
 class StaffMember extends Employee {
-  constructor(name, surename, photo, email, staffID) {
+  constructor(name, surename, photo, email) {
     super(name, surename)
-      this.photo = photo
-      this.email = email
-      this.staffID = staffID
-    }
-  staffMemberisLate() {
-    setTimeout(() => {
-      console.log(`${this.name} ${this.surename} is late`)
-    }, 1000)
+    this.photo = photo
+    this.email = email
   }
 }
 class DeliverDriver extends Employee {
   constructor(name, surename, vehicle, telephone, deliveryAddress){
     super(name, surename)
-      this.vehicle = vehicle
-      this.telephone = telephone
-      this.deliveryAddress = deliveryAddress
+    this.vehicle = vehicle
+    this.telephone = telephone
+    this.deliveryAddress = deliveryAddress
   }
 }
 
 async function staffUserGet() {
   const response = await fetch('https://randomuser.me/api/?results=5&inc=name,email,picture,id')
   staff = await response.json()
-  console.log(staff)
   staff = await staff.results.map(staffMember => 
-  new StaffMember(staffMember.name.first, staffMember.name.last, staffMember.picture.medium, staffMember.email, staffMember.id.value))
+  new StaffMember(staffMember.name.first, staffMember.name.last, staffMember.picture.medium, staffMember.email))
   tableData=""
   staff.map(staffMembers => {
     tableData += 
-    `<tr id="${staffMembers.staffID}" class="stafftablerow">
+    `<tr class="stafftablerow">
     <td id="photo"><img src="${staffMembers.photo}"></td>
     <td id="name">${staffMembers.name}</td>
     <td id="surename">${staffMembers.surename}</td>
@@ -44,30 +37,31 @@ async function staffUserGet() {
     <td id="outtime"></td>
     <td id="duration"></td>
     <td id="expectedreturntime"></td>
-    <td id="late"></td>
     </tr>`})
   document.getElementById("staffmembers").innerHTML = tableData //NOT WORKING WITH JQUERY FOR SOME REASON
 }
 
 $(document).ready(function(){
-  $(document.body).on('click', '.stafftablerow', function(){$(this).toggleClass('selected')})
+ $(document.body).on('click', '.stafftablerow', function(){$(this).toggleClass('selected')}) //NOT WORKING WITH $().click FOR SOME REASON
 })
+
 
 $("#clockout").click(function(){
   absence = parseInt(prompt("Please enter the duration of absence in minutes"))
   selected = $(".selected")
-  selected.find
   selected.find("#status").text("Out")
-  selected.find("#duration").text(absence)
+  selected.find("#duration").text(moment().startOf('day').add(absence, 'minutes').format('HH:mm'))
   selected.find("#outtime").text(moment().format('HH:mm'))
+  selected.find("#expectedreturntime").text(moment().add(absence, 'minutes').format('HH:mm')) 
   staffName = selected.find("#name").text()
   staffSurename = selected.find("#surename").text()
   staffPhoto = selected.find("img").attr("src")
   notifyDelay(staffName, staffSurename, staffPhoto, absence)
 })
 
+
 function notifyDelay(staffName, staffSurename, staffPhoto, absence) {
-  setTimeout(() => {
+  timeoutID = setTimeout(() => {
   toastContent =
   `<div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
   <div class="toast-header">
@@ -76,10 +70,13 @@ function notifyDelay(staffName, staffSurename, staffPhoto, absence) {
   <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
   </div>`
-  document.getElementById("toastConteiner").innerHTML = toastContent
+  document.getElementById("toastConteiner").innerHTML = toastContent //NOT WORKING WITH JQUERY FOR SOME REASON
   $('.toast').toast('show')
+  $(`tr[timeoutid="${timeoutID}"]`).removeAttr("timeoutid")
   }, absence*100)
+  $(".selected").attr("timeoutid", timeoutID)
 }
+
 
 $("#clockin").click(function(){
   selected = $(".selected")
@@ -87,7 +84,11 @@ $("#clockin").click(function(){
   selected.find("#outtime").text("")
   selected.find("#duration").text("")
   selected.find("#expectedreturntime").text("")
+  timeoutID = selected.attr("timeoutid")
+  $(`tr[timeoutid="${timeoutID}"]`).removeAttr("timeoutid")
+  clearTimeout(timeoutID)
 })
+
 
 $(document).ready(function notifyDelay(){
   $("#liveToastBtn").click(function(){
@@ -97,8 +98,6 @@ $(document).ready(function notifyDelay(){
 });
 
 staffUserGet()
-
-
 
 
 
